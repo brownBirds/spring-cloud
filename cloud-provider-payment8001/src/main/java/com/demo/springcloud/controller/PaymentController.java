a543d8f4325e4745a179a0e0c0a5241373c5ecfe
wangyuhao <842441141@qq.com>
2020-06-01 23:12:24 +0800

    '新增模块'

--- cloud-provider-payment8001/src/main/java/com/demo/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/demo/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.demo.springcloud.entities.CommonResult;
 import com.demo.springcloud.entities.Payment;
 import com.demo.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,15 +20,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
 
     public CommonResult creat( @RequestBody Payment payment){
         int result = paymentService.creat(payment);
         log.info("****插入结果："+result);
 
-        if (result>0){
-            return  new CommonResult(200,"插入数据库成功",result);
-        }else {
+        if(result > 0){
+            return  new CommonResult(200,"插入数据库成功,serverPort: " + serverPort,result);
+        } else {
             return new CommonResult(444,"插入数据库失败",null);
         }
 
