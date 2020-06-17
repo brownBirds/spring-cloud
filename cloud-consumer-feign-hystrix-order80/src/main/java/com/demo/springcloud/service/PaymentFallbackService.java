350494903f810ffc12a650eb90d16dc9d30476e3
wangyuhao <842441141@qq.com>
2020-06-17 23:21:56 +0800

    6/17 new hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/demo/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,21 @@
+package com.demo.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: WangYuhao
+ * @Date:2020/6/17 20:44
+ */
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "----PaymentFallbackService fall back paymentInfo_OK,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_Timeout(Integer id) {
+        return "----PaymentFallbackService fall back paymentInfo_Timeout,o(╥﹏╥)o";
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/demo/springboot/PaymentHystrixMain8001.java
@@ -0,0 +1,20 @@
+package com.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: WangYuhao
+ * @Date:2020/6/17 20:18
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}
